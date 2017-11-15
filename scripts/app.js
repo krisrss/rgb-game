7ee9f4b1c97495263ccd01fb15f19604df8f8fe5
Kristaps R <kristapsrss@gmail.com>
2017-11-15 15:48:45 +0200

    Add attempt limit, that user can take to guess the color

--- scripts/app.js
+++ scripts/app.js
@@ -3,14 +3,17 @@
 const gameContent = document.querySelector("#color-tiles");
 const colorToGuess = document.querySelector("#correct-color");
 const header = document.querySelector("#header");
+const triesCounter = document.querySelector("#tries");
 
 let GameBoard = {
     numberOfTiles : 9,
     correctColor : "",
+    gameActive: false,
+    triesLeft: 3,
     
     //Add color tiles to board
     addTiles : function(){
-        for(let i = 0; i < 9; i++){
+        for(let i = 0; i < this.numberOfTiles; i++){
             gameContent.appendChild(ColorTile.createTile());
         };
     },
@@ -18,7 +21,7 @@ let GameBoard = {
     //Set a color, that user has to guess
     setColorToGuess: function(){
         const selectTiles = 
-        document.querySelectorAll(".color-tile")[randomNr(9) - 1];
+        document.querySelectorAll(".color-tile")[randomNr(this.numberOfTiles) - 1];
         this.correctColor = selectTiles.style.backgroundColor;
     }
 };
@@ -55,9 +58,17 @@ const randomNr = (maxNumber) =>
 function validateUserGuess(element, color){
     if(element.style.backgroundColor === color){
         header.textContent = "CORRECT";
+        GameBoard.gameActive = false;
     }
     else{
         element.style.visibility = "hidden";
+        GameBoard.triesLeft--;
+        triesCounter.textContent = GameBoard.triesLeft;
+    }
+
+    if(GameBoard.triesLeft === 0){
+        header.textContent = "GAME OVER";
+        GameBoard.gameActive = false;
     }
 };
 
@@ -65,24 +76,24 @@ function validateUserGuess(element, color){
 function winCondition(guessColor){
     gameContent.addEventListener("click", (el)=>{
         const selectedEl = el.target;
-        if(selectedEl.className === "color-tile"){
+        if(selectedEl.className === "color-tile" && GameBoard.gameActive === true){
             validateUserGuess(selectedEl,guessColor)
         }            
     });            
 };
 
 
-
 //Initialize the game board with game elements
 function init(){
+    GameBoard.gameActive = true;
     GameBoard.addTiles();
     GameBoard.setColorToGuess();
 
     colorToGuess.textContent = GameBoard.correctColor; 
+    triesCounter.textContent = GameBoard.triesLeft;
 
     winCondition(GameBoard.correctColor);
 };
-
 
 init();
 
