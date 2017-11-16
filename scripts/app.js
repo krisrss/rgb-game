24fb47cf38f35f05275b667938865bf5b571a7fd
Kristaps R <kristapsrss@gmail.com>
2017-11-16 17:48:40 +0200

    Add replay button

--- scripts/app.js
+++ scripts/app.js
@@ -4,6 +4,8 @@ const gameContent = document.querySelector("#color-tiles");
 const colorToGuess = document.querySelector("#correct-color");
 const header = document.querySelector("#header");
 const triesCounter = document.querySelector("#tries");
+const replayButton = document.querySelector("#replay-tab");
+
 
 const timerCountdown = document.querySelector("#countdown");
 
@@ -12,12 +14,20 @@ let GameBoard = {
     correctColor : "",
     gameActive: false,
     triesLeft: 3,
-    countDownTime: 15,
+    countDownTime: 50,
     
     //Add color tiles to board
     addTiles : function(){
         for(let i = 0; i < this.numberOfTiles; i++){
             gameContent.appendChild(ColorTile.createTile());
+        };
+    },
+    
+    //Applies random colors to tiles
+    applyColors : function(){
+        const selectTiles = document.querySelectorAll(".color-tile");        
+        for(let i = 0; i < this.numberOfTiles; i++){
+            selectTiles[i].style.backgroundColor = ColorTile.randomColor();
         };
     },
     
@@ -33,7 +43,6 @@ let GameBoard = {
 let ColorTile = {
     createTile : function(){
         const colorTile = createElement("div","className","color-tile");
-        colorTile.style.backgroundColor = this.randomColor();
         return colorTile;
     },
 
@@ -65,12 +74,12 @@ let CountDownTimer = {
     ticker : function(){
         if(GameBoard.gameActive === true){
             GameBoard.countDownTime--;
-            timerCountdown.textContent = GameBoard.countDownTime;
-        };
+            timerCountdown.textContent = GameBoard.countDownTime; 
+        }
         if(GameBoard.countDownTime === 0){
             header.textContent = "TIME OVER";
-            GameBoard.gameActive = false;
-        };
+            GameBoard.gameActive = false;  
+        }
     }
 };
 
@@ -87,8 +96,8 @@ const randomNr = (maxNumber) =>
     Math.floor(Math.random()*maxNumber) + 1;
 
 //Validate the guess made by user, and hide wrong guesses
-function validateUserGuess(element, color){
-    if(element.style.backgroundColor === color){
+function validateUserGuess(element){
+    if(element.style.backgroundColor === GameBoard.correctColor){
         header.textContent = "CORRECT";
         GameBoard.gameActive = false;
     }
@@ -105,31 +114,58 @@ function validateUserGuess(element, color){
 };
 
 //Set click event on each tile, that checks if game conditions are met
-function winCondition(guessColor){
+function winCondition(){
     gameContent.addEventListener("click", (el)=>{
+        el.stopImmediatePropagation();        
         const selectedEl = el.target;
         if(selectedEl.className === "color-tile" && GameBoard.gameActive === true){
-            validateUserGuess(selectedEl,guessColor)
+            validateUserGuess(selectedEl)
         }            
     });            
 };
 
+//Set default game values for game board
+function setGameState(tileNr,gameState,triesNr,timeNr){
+    GameBoard.numberOfTiles = tileNr;
+    GameBoard.gameActive = gameState;
+    GameBoard.triesLeft = triesNr;
+    GameBoard.countDownTime = timeNr;
+};
+
+//Update the game board with game elements
+function setUpGame(){
+    GameBoard.applyColors();
+    GameBoard.setColorToGuess();
+    header.textContent = "Guess the color - " + GameBoard.correctColor;    
+    triesCounter.textContent = GameBoard.triesLeft;
+    timerCountdown.textContent = GameBoard.countDownTime;
+    winCondition();
+};
+
+//Reset hidden color tiles
+function resetColorTiles(){
+    const selectTiles = document.querySelectorAll(".color-tile");    
+    for(let i = 0; i < GameBoard.numberOfTiles; i++){
+        selectTiles[i].style.visibility = "visible";
+    };
+}
+
+//Resets the game, allowing player to play again
+replayButton.addEventListener("click",function(){
+    resetColorTiles();
+    setGameState(3,true,2,30);
+    setUpGame();
+    CountDownTimer.reset();
+    
+});
 
 //Initialize the game board with game elements
 function init(){
-    GameBoard.gameActive = true;
+    setGameState(3,true,2,30);
     GameBoard.addTiles();
-    GameBoard.setColorToGuess();
     CountDownTimer.start();
-
-    colorToGuess.textContent = GameBoard.correctColor; 
-    triesCounter.textContent = GameBoard.triesLeft;
-    timerCountdown.textContent = GameBoard.countDownTime;
-
-    winCondition(GameBoard.correctColor);
+    setUpGame();
 };
 
+
 init();
-
-
-
