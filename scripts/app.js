78f53558a9698f5825ab38e7cfa1fe1c6fd14c1d
Kristaps R <kristapsrss@gmail.com>
2017-11-17 19:57:44 +0200

    Add difficulty options

--- scripts/app.js
+++ scripts/app.js
@@ -5,16 +5,22 @@ const colorToGuess = document.querySelector("#correct-color");
 const header = document.querySelector("#header");
 const triesCounter = document.querySelector("#tries");
 const replayButton = document.querySelector("#replay-tab");
-
+const menuButton = document.querySelector("#menu-tab");
 
 const timerCountdown = document.querySelector("#countdown");
 
+const gameMenu = document.querySelector("#difficulty-panel");
+const easyButton = document.querySelector("#easy-button");
+const mediumButton = document.querySelector("#medium-button");
+const hardButton = document.querySelector("#hard-button");
+
 let GameBoard = {
-    numberOfTiles : 9,
+    numberOfTiles : 0,
     correctColor : "",
     gameActive: false,
-    triesLeft: 3,
-    countDownTime: 50,
+    triesLeft: 0,
+    countDownTime: 0,
+    difficultyMode: "",
     
     //Add color tiles to board
     addTiles : function(){
@@ -57,14 +63,18 @@ let ColorTile = {
 
 let CountDownTimer = {
     timerStore : null,
+    time : 0,
 
     //Start the timer
     start : function(time){
+        GameBoard.countDownTime = this.time;
         this.timerStore = setInterval(this.ticker, 1000);
     },
 
     //Reset timer to default values
     reset : function(){
+        GameBoard.countDownTime = this.time;
+        
         clearInterval(this.timerStore);
         this.timerStore = null;
         return this.start();
@@ -129,7 +139,7 @@ function setGameState(tileNr,gameState,triesNr,timeNr){
     GameBoard.numberOfTiles = tileNr;
     GameBoard.gameActive = gameState;
     GameBoard.triesLeft = triesNr;
-    GameBoard.countDownTime = timeNr;
+    CountDownTimer.time = timeNr;
 };
 
 //Update the game board with game elements
@@ -144,28 +154,63 @@ function setUpGame(){
 
 //Reset hidden color tiles
 function resetColorTiles(){
-    const selectTiles = document.querySelectorAll(".color-tile");    
-    for(let i = 0; i < GameBoard.numberOfTiles; i++){
-        selectTiles[i].style.visibility = "visible";
-    };
-}
+    var selectTiles = document.querySelectorAll(".color-tile");
+    selectTiles.forEach(function(element) {
+        gameContent.removeChild(element)
+    }, this);
+};
 
-//Resets the game, allowing player to play again
-replayButton.addEventListener("click",function(){
+menuButton.addEventListener("click",function(){
+    GameBoard.gameActive = false;
     resetColorTiles();
-    setGameState(3,true,2,30);
-    setUpGame();
-    CountDownTimer.reset();
+    gameMenu.style.visibility = "visible";
     
 });
 
+//Resets the game, allowing player to play again, with same settings
+replayButton.addEventListener("click",function(){
+
+    if(GameBoard.difficultyMode === "Easy"){
+        setGameState(3,true,2,40);
+    }
+    else if(GameBoard.difficultyMode === "Medium"){
+        setGameState(6,true,3,25);    
+    }
+    else if(GameBoard.difficultyMode === "Hard"){
+        setGameState(9,true,3,20);    
+    }
+
+    init();
+});
+
+
+
 //Initialize the game board with game elements
 function init(){
-    setGameState(3,true,2,30);
+    resetColorTiles();
     GameBoard.addTiles();
-    CountDownTimer.start();
+    CountDownTimer.reset();   
     setUpGame();
 };
 
 
-init();
+easyButton.addEventListener("click", function(){
+    gameMenu.style.visibility = "hidden";
+    GameBoard.difficultyMode = "Easy";
+    setGameState(3,true,2,40);    
+    init();
+});
+
+mediumButton.addEventListener("click", function(){
+    gameMenu.style.visibility = "hidden";
+    GameBoard.difficultyMode = "Medium";
+    setGameState(6,true,3,25);    
+    init();
+});
+
+hardButton.addEventListener("click", function(){
+    gameMenu.style.visibility = "hidden";
+    GameBoard.difficultyMode = "Hard";
+    setGameState(9,true,3,20);    
+    init();
+});
